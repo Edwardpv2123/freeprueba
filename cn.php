32b7ea85f9eeea57a4db751e242fdd8789dbb98c
Edwardpv2123 <62851630+Edwardpv2123@users.noreply.github.com>
2020-04-22 19:26:02 -0500

    Add files via upload

--- cn.php
+++ cn.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("https://edwardpv2123.github.io/freeprueba/", "root", "", "bd_prueba");
+$conexion = mysqli_connect("https://github.com/Edwardpv2123/freeprueba.git", "root", "", "bd_prueba");
 //if (!$conexion) {
 //    echo 'Error al conectar a la base de datos';
 //}
